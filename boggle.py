bf08f98fd59071a1a29d03b88261dc58c7c8e417
EC2 Default User <ec2-user@ip-172-31-83-195.ec2.internal>
2019-10-16 15:07:44 +0000

    runned test to check grid

---
+++ boggle.py
@@ -0,0 +1,9 @@
+def make_grid(width,height):
+    """
+    Creates a grid that will hold all of tiles
+    for a boggle game
+    """
+    return {(row,col): ' ' for row in range(height)
+        for col in range(width)
+    }
+    
